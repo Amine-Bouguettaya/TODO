bfc928c8625431d84bbba1d27e28cb52a3521b7d
Amine-Bouguettaya <aminebouguettaya5@gmail.com>
2024-11-15 14:47:32 +0100

    todo list in js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const addBtn = document.querySelector('#btn'); //return the first elements that match #btn (button "add a task")
+const taskCard = document.querySelector(".todoCard"); // get the card it slef (thextarea & delete button)
+const count = document.querySelector("#count");
+const tasksContainer = document.querySelector("#todoCards"); // get the div that contains the differents cards
+addBtn.addEventListener('click', addTask); //call function addTask when clic
+const rmBtn = document.querySelector(".delBtn"); //same as addBtn but for delete button (fa-trash)
+rmBtn.addEventListener('click', function () { //delete default task on click
+    count.innerHTML = tasksContainer.querySelectorAll(".todoCard").length
+    deleteTask(taskCard); //target the task
+});
+
+// console.log(count.innerHTML);
+
+function addTask () {
+    const newTask = taskCard.cloneNode(true); //clone first taskcard to a new variable
+    const newDelBtn = newTask.querySelector('.delBtn');
+    const NewTextArea = newTask.querySelector('.task'); // get the textarea to add the text on next line
+    
+    NewTextArea.value = "New Task"; //add text "New Task" to the new task
+    tasksContainer.appendChild(newTask); //add the new task into the div (in this case : todoCards)
+    newDelBtn.addEventListener('click', function () { // add eventlistener to delete the newTask
+        deleteTask(newTask); //target the new task
+    });
+    count.innerHTML = tasksContainer.querySelectorAll(".todoCard").length;
+}
+
+function deleteTask(task) {
+    count.innerHTML = tasksContainer.querySelectorAll(".todoCard").length;
+    task.remove(); //remove the task
+}
+
+// function counter(tasksContainer) {
+//     const nb = tasksContainer.querySelectorAll("#todoCards").length;
+//     return nb;
+// }
